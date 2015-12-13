f6e3e839116be727e89ec3c636f143c738ea082d
Craig Weysham <battlemaster2012@gmail.com>
2015-12-13 02:16:20 -0600

    Initialization

---
+++ BackgroundChanger/BackgroundChangerProgram.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackgroundChanger
+{
+    static class BackgroundChangerProgram
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new BackgroundChanger()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
